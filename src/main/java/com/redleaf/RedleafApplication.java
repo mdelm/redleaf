cab3117cba30855df3ce96d57e1dbb746e16c146
md-elm <elmehammedi.mohamed@gmail.com>
2020-12-12 23:36:30 +0100

    Project Object

--- src/main/java/com/redleaf/RedleafApplication.java
+++ src/main/java/com/redleaf/RedleafApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.redleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RedleafApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(RedleafApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/redleaf/domain/Project.java
@@ -0,0 +1,5 @@
+package com.redleaf.domain;
+
+public class Project {
+
+}
