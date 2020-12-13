18aa0820a47e37b9e45ad5ae246c1b09289ef692
md-elm <elmehammedi.mohamed@gmail.com>
2020-12-13 23:59:02 +0100

    Style Dashboard, Navbar, Project

---
+++ react-client/src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Dashboard from "../Dashboard/index";
+import "../../assets/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
--- react-client/src/components/Dashboard/index.jsx
+++ react-client/src/components/Dashboard/index.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
-import Header from "../Header";
+import Navbar from "../Navbar";
 import Project from "../Project";
 
 class Dashboard extends Component {
 	render() {
-		return <div>
-			<Header />
-			<div className="alert alert-success">Alert</div>
-			<Project />
-			<Project />
-			<Project />
-		</div>
+		return <div className="projects">
+			<Navbar />
+	        <div className="container">
+	            <div className="row">
+	                <div className="col-md-12">
+	                    <h1 className="display-4 text-center">Projects</h1>
+	                    <br />
+	                    <a href="ProjectForm.html" className="btn btn-lg btn-info">
+	                        Create a Project
+	                    </a>
+	                    <br />
+	                    <hr />
+
+	                    <Project />
+	                    <Project />
+
+	                </div>
+	            </div>
+	        </div>
+	    </div>
 	}
 }
 
