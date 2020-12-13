394bc70537664f3ad376a6bec45ad9150536870c
md-elm <elmehammedi.mohamed@gmail.com>
2020-12-13 21:07:13 +0100

    Update an existing project

--- src/main/java/com/redleaf/web/ProjectController.java
+++ src/main/java/com/redleaf/web/ProjectController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,16 @@ public class ProjectController {
     @DeleteMapping("/{projectId}")
     public ResponseEntity<?> deleteProject(@PathVariable(name = "projectId") String projectId) {
         projectService.deleteProject(projectId);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+    
+    @PutMapping
+    public ResponseEntity<?> updateProject(@Valid @RequestBody Project project, BindingResult result) {
+        
+        if (result.hasErrors())
+            throw new ValidationException(result);
+        
+        projectService.save(project);
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
